bf66af3a48240c4038e578e5949f2e28c8cbdb6e
[Leggio] <[leggio989898@gmail.com]>
2022-05-07 17:10:39 +0200

    Add: exercise 3

---
+++ Esercizi/EserciziJS/Esercizio3/exercise-3.js
@@ -0,0 +1,7 @@
+const students = ['Paul', 'George', 'Lucas'];
+
+function addStudent(student) {
+  return students.push(student); // Return non è necessario, ma mi è comodo" // 
+}
+addStudent('Marco');
+console.log(students); 
