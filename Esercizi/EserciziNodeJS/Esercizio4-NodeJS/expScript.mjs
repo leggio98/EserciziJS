16f8a026ce13e2972db350a0d599719da4d8fbfa
[Leggio] <[leggio989898@gmail.com]>
2022-09-09 12:16:28 +0200

    Add Exercise 4

---
+++ Esercizi/EserciziNodeJS/Esercizio4-NodeJS/expScript.mjs
@@ -0,0 +1,5 @@
+function getMessage(message) {
+    console.log(`Message: ${message}`)
+}
+
+export default getMessage
---
+++ Esercizi/EserciziNodeJS/Esercizio4-NodeJS/impScript.mjs
@@ -0,0 +1,3 @@
+import getMessage from "./expScript.mjs";
+
+getMessage("ciao")
