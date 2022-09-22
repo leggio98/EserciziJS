c133873278b3dd58728c1a39b0dbfb478b07610b
[Leggio] <101880593+leggio98@users.noreply.github.com>
2022-09-22 16:19:10 +0200

    Add exercise 8

---
+++ Esercizi/EserciziNodeJS/Esercizio7-NodeJS/asciiArt.js
@@ -0,0 +1,21 @@
+const asciiText = require('figlet');
+
+asciiText.text(
+    "asciiText!",
+    {
+        font: "Ghost",
+        horizontalLayout: "default",
+        verticalLayout:"default",
+        width: 80,
+        whitespaceBreak: true,
+    },
+
+    function(err,data)  {
+        if (err) {
+            console.log("Something went wrong...");
+            console.dir(err);
+            return;
+        }
+        console.log(data);
+    }
+);
